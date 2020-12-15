60ff8d43eaac2951383f20dcb8fe18c7c7a65f50
DenysVodzyk <vodzyk@gmail.com>
2020-12-15 15:50:31 -0500

    Added functionality to the class Account and added class SavingAccount

--- Week6_Solutions/src/Account.java
+++ Week6_Solutions/src/Account.java
@@ -1,2 +1,49 @@
-public class Account {
+public abstract class Account {
+    private int accNumber;
+    private double balance;
+    private String owner;
+
+    public Account() {
+    }
+
+    public Account(int accNumber, String owner, double balance) {
+        this.accNumber = accNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public int getAccNumber() {
+        return accNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public abstract boolean withdraw(double amount);
+
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            setBalance(getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accNumber=" + accNumber +
+                ", balance=" + balance +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
 }
---
+++ Week6_Solutions/src/SavingAccount.java
@@ -0,0 +1,39 @@
+public class SavingAccount extends Account {
+    private double minimumBalance;
+
+
+    public SavingAccount() {
+    }
+
+    public SavingAccount(int accNumber, String owner, double balance, double minimumBalance) {
+        super(accNumber, owner, balance);
+        this.minimumBalance = minimumBalance;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        double remainingBalance = super.getBalance() - amount;
+        if (amount <= super.getBalance()) {
+            if (remainingBalance < getMinimumBalance()) {
+                System.out.println("Extra charge of $1.5 will be applied to your account.");
+                super.setBalance(remainingBalance - 1.5);
+            } else {
+                super.setBalance(remainingBalance);
+            }
+            return true;
+        }
+        System.out.println("You don't have enough money on your saving account.");
+        return false;
+    }
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "SavingAccount{" + super.toString() + " " +
+                "minimumBalance=" + minimumBalance +
+                '}';
+    }
+}

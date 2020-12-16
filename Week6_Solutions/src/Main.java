75e00e81239b9993daf1f4e40d3b7fab11efe379
DenysVodzyk <vodzyk@gmail.com>
2020-12-16 18:34:26 -0500

    Added more cases to test the functionality

--- Week6_Solutions/src/Main.java
+++ Week6_Solutions/src/Main.java
@@ -9,17 +9,20 @@ public class Main {
 
         account1.withdraw(200);
         System.out.println(account1.getBalance());
-        account1.withdraw(1000000);
+        account1.withdraw(999995);
+        System.out.println(account1.getBalance());
+        account1.withdraw(3);
         System.out.println(account1.getBalance());
 
-//
-//        AccountManager accountManager = new AccountManager();
-//        accountManager.addAccount(111, "Elon Musk", 1000000, 1000);
-//        accountManager.addAccount(222, "Warren Buffett", 2000000, 2000);
-//
-//
-//        System.out.println(accountManager.printAccountDetails(111));
-//        System.out.println(accountManager);
+
+        AccountManager accountManager = new AccountManager();
+        accountManager.addAccount(111, "Elon Musk", 1000000, 1000);
+        accountManager.addAccount(222, "Warren Buffett", 2000000, 2000);
+        accountManager.addAccount(222, "Elon Musk", 1000000, 1000);
+
+        System.out.println(accountManager);
+        System.out.println(accountManager.listAllAccounts());
+        System.out.println("Account Details: " + accountManager.printAccountDetails(111));
 
 
     }

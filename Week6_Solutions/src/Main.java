c7207465f1b93eccc13d5bc7d9040168e3640485
DenysVodzyk <vodzyk@gmail.com>
2020-12-16 21:11:07 -0500

    Updated Main class

--- Week6_Solutions/src/Main.java
+++ Week6_Solutions/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Account account1 = new SavingAccount(111, "Elon Musk", 1000000, 1000);
+        Account account2 = new SavingAccount(222, "Warren Buffett", 2000000, 2000);
+        Account account3 = new SavingAccount(222, "Elon Musk", 1000000, 1000);
 
         account1.deposit(200);
         System.out.println(account1.getBalance());
@@ -8,6 +10,7 @@ public class Main {
         System.out.println(account1.getBalance());
 
         account1.withdraw(200);
+
         System.out.println(account1.getBalance());
         account1.withdraw(999995);
         System.out.println(account1.getBalance());
@@ -16,14 +19,13 @@ public class Main {
 
 
         AccountManager accountManager = new AccountManager();
-        accountManager.addAccount(111, "Elon Musk", 1000000, 1000);
-        accountManager.addAccount(222, "Warren Buffett", 2000000, 2000);
-        accountManager.addAccount(222, "Elon Musk", 1000000, 1000);
+        accountManager.addAccount(account1);
+        accountManager.addAccount(account2);
+        accountManager.addAccount(account3);
 
         System.out.println(accountManager);
         System.out.println(accountManager.listAllAccounts());
         System.out.println("Account Details: " + accountManager.printAccountDetails(111));
-
 
     }
 }

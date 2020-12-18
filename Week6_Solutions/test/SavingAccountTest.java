904259434b65a3d363e274cb1f784c97609ef1f3
DenysVodzyk <vodzyk@gmail.com>
2020-12-17 19:11:43 -0500

    Updating unit test with methods

--- Week6_Solutions/test/SavingAccountTest.java
+++ Week6_Solutions/test/SavingAccountTest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class SavingAccountTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SavingAccountTest {
+
+    @Test
+    public void withdrawTest() {
+        SavingAccount savingAccountTest = new SavingAccount(1, "owner", 1000, 100);
+        boolean resultTest = savingAccountTest.withdraw(100);
+        assertTrue(resultTest);
+    }
+
+    @Test
+    public void withdrawTest2() {
+        SavingAccount savingAccountTest = new SavingAccount(1, "owner", 1000, 100);
+        boolean resultTest = savingAccountTest.withdraw(999);
+        assertEquals(false, resultTest);
+    }
+
+
 }
